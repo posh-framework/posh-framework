0fa00cf9776f785f7494620b9c1117b28e3dd105
carsongee_cp <SND\carsongee_cp>
2009-06-05 04:11:30 +0000

    Added Number, List, File, and Directory types to parameter types for ticket 22678.

--- trunk/scripts/frameworkharness.ps1
+++ trunk/scripts/frameworkharness.ps1
@@ -9,6 +9,15 @@ $param | add-member NoteProperty Type "String"
 $ScriptParameters += $param
 
 $param = New-Object PSObject
+$param | add-member NoteProperty Name 'FolderChooser'
+$param | add-member NoteProperty Value ''
+$param | add-member NoteProperty Prompt 'For testing the folder chooser'
+$param | add-member NoteProperty Type 'folder'
+$param | add-member NoteProperty Display 'Test Folder'
+$ScriptParameters += $param
+
+
+$param = New-Object PSObject
 $param | add-member NoteProperty Name 'CheckBox'
 $param | add-member NoteProperty Value ''
 $param | add-member NoteProperty Prompt 'For testing the checkbox'
@@ -16,12 +25,38 @@ $param | add-member NoteProperty Type 'Boolean'
 $param | add-member NoteProperty Display 'Test CheckBox'
 $ScriptParameters += $param
 
-$ScriptDescription = 'Script for displaying a parameter'
+$param = New-Object PSObject
+$param | add-member NoteProperty Name 'ListChooser'
+$param | add-member NoteProperty Value 'Peas|Carrots|Cabbage|Beats|Onions'
+$param | add-member NoteProperty Prompt 'For testing the list/choice type'
+$param | add-member NoteProperty Type 'List'
+$param | add-member NoteProperty Display 'Favorite Vegetable'
+$ScriptParameters += $param
+
+$param = New-Object PSObject
+$param | add-member NoteProperty Name 'FileChooser'
+$param | add-member NoteProperty Value ''
+$param | add-member NoteProperty Prompt 'For testing the file chooser'
+$param | add-member NoteProperty Type 'File'
+$param | add-member NoteProperty Display 'Test File'
+$ScriptParameters += $param
+
+$param = New-Object PSObject
+$param | add-member NoteProperty Name 'Spinner'
+$param | add-member NoteProperty Value ''
+$param | add-member NoteProperty Prompt 'For testing the number spinner'
+$param | add-member NoteProperty Type 'Number'
+$param | add-member NoteProperty Display 'Test Number'
+$ScriptParameters += $param
+
+
+
+$ScriptDescription = 'Script for showing features'
 $ScriptThreaded = $True
 $ScriptADSObjectType = 'computer'
 $ScriptBlock = 
 {
-    write-output $displayText
+    write-output "Display Text: $displayText"
     if($CheckBox)
     {
 	write-output "Checkbox is True"
@@ -30,5 +65,8 @@ $ScriptBlock =
     {
 	write-output  "Checkbox is False"
     }
-	
+    write-output ("Spinner times 5 = " + (5*$Spinner))
+    write-output "FilePath: $FileChooser"
+    write-output "FolderPath: $FolderChooser"
+    write-output "Favorite Vegetable: $ListChooser"
 }
